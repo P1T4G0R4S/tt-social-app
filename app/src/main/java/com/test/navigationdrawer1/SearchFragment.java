e869e26f35d1906593f2a73152fcac98db7f696f
Osvaldo Frias <osvaldourielfp@gmail.com>
2017-11-05 11:43:10 -0600

    Add animation to fragment image

--- app/src/main/java/com/test/navigationdrawer1/SearchFragment.java
+++ app/src/main/java/com/test/navigationdrawer1/SearchFragment.java
@@ -1,18 +1,29 @@
 package com.test.navigationdrawer1;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.test.navigationdrawer1.Network.DeviceType;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
+    MainActivity activity;
+    SharedPreferences pref;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -26,8 +37,30 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        activity = (MainActivity) getActivity();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        pref = activity.getSharedPreferences("Options", MODE_PRIVATE);
+        int deviceTypePref = pref.getInt("devicetype", DeviceType.EMITTER.getCode());
+        DeviceType deviceType = DeviceType.get(deviceTypePref);
+        Log.e("TEST", "" + deviceType);
+        ImageView myImageView= (ImageView)activity.findViewById(R.id.broadcast);
+
+        if (deviceType == DeviceType.ACCESS_POINT) {
+            Animation myFadeInAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.fadein);
+            myImageView.startAnimation(myFadeInAnimation); //Set animation to your ImageView
+            myImageView.setVisibility(View.VISIBLE);
+        } else {
+            myImageView.setVisibility(View.GONE);
+        }
     }
 
     @Override
